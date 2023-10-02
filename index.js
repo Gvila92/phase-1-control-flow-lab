2a38cce825270a01b81938c7735d6c4f83efebfc
George vila <gvila92@gmail.com>
2023-10-02 16:05:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  if (tipAmount > 15) {
+    return "Thank you so much.";
+  } else if (tipAmount > 0) {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
